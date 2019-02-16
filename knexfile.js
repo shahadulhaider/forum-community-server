5ad5efdc79cf42ecdbc0615425d0603827a1195f
Shahadul Haider <shahadul.haider@gmail.com>
2019-02-16 19:37:51 +0600

    updated production connection

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-
   development: {
     client: 'postgresql',
     connection: {
@@ -20,18 +19,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: process.env.DATABASE_URL
   }
-
 };
